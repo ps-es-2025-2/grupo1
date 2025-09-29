762dbbc73d69b2ddcf291246aa7ffe4c642a841e
Gabriel Rodrigues <61525555+Gabriellrs@users.noreply.github.com>
2025-09-29 16:54:28 -0300

    add view da tela principal do motorista
    O código-fonte atual implementa a tela principal do motorista (DriverDashboard.fxml). A classe Main.java inicia a aplicação e carrega essa tela. O DriverDashboardController.java já contém os elementos da interface (listas de passageiros, botões) e ações com lógicas ainda a serem implementadas (indicadas por System.out.println).

---
+++ src/main/java/br/com/appvagasvan/Main.java
@@ -0,0 +1,35 @@
+
+package br.com.appvagasvan;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Classe principal que inicia a aplicação JavaFX.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Carrega o arquivo FXML da tela do motorista a partir da pasta resources
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/appvagasvan/DriverDashboard.fxml"));
+        Parent root = loader.load();
+
+        // Configura a cena principal
+        Scene scene = new Scene(root, 800, 600);
+
+        // Configura o palco (janela principal)
+        primaryStage.setTitle("Painel do Motorista - App Vagas Van");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
