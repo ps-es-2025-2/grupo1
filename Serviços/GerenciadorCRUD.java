d935993d0a916dd9daa5dcffae111a98a66a770f
lauramvg1821 <145988821+lauramvg1821@users.noreply.github.com>
2025-09-30 20:30:39 -0300

    Delete Serviços directory

--- Serviços/GerenciadorCRUD.java
+++
@@ -1,7 +0,0 @@
-public interface GerenciadorCRUD<T> {
-    T criar(T entidade);
-    java.util.List<T> buscarTodos();
-    T buscarPorId(int id);
-    T atualizar(T entidade);
-    void deletar(int id);
-}
