c2fe3f7c5ad670860a6f42be5abd156af6d52043
lauramvg1821 <145988821+lauramvg1821@users.noreply.github.com>
2025-09-30 20:26:40 -0300

    Crud

---
+++ Serviços/GerenciadorCRUD.java
@@ -0,0 +1,7 @@
+public interface GerenciadorCRUD<T> {
+    T criar(T entidade);
+    java.util.List<T> buscarTodos();
+    T buscarPorId(int id);
+    T atualizar(T entidade);
+    void deletar(int id);
+}
